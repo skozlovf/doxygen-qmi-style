e0c7090c48c1aa819270a03cebd3cfbe65ea8dc3
Sergey Kozlov <skozlovf@gmail.com>
2011-12-27 02:48:00 +0700

    Added example documentation of the libxml++.

---
+++ libxmlpp/navtree.js
@@ -0,0 +1,344 @@
+var NAVTREE =
+[
+  [ "libxml++", "index.html", [
+    [ "Class List", "annotated.html", [
+      [ "xmlpp::Attribute", "classxmlpp_1_1Attribute.html", null ],
+      [ "xmlpp::SaxParser::Attribute", "structxmlpp_1_1SaxParser_1_1Attribute.html", null ],
+      [ "xmlpp::SaxParser::AttributeHasName", "structxmlpp_1_1SaxParser_1_1AttributeHasName.html", null ],
+      [ "xmlpp::CdataNode", "classxmlpp_1_1CdataNode.html", null ],
+      [ "xmlpp::CommentNode", "classxmlpp_1_1CommentNode.html", null ],
+      [ "xmlpp::ContentNode", "classxmlpp_1_1ContentNode.html", null ],
+      [ "xmlpp::Document", "classxmlpp_1_1Document.html", null ],
+      [ "xmlpp::DomParser", "classxmlpp_1_1DomParser.html", null ],
+      [ "xmlpp::Dtd", "classxmlpp_1_1Dtd.html", null ],
+      [ "xmlpp::Element", "classxmlpp_1_1Element.html", null ],
+      [ "xmlpp::EntityReference", "classxmlpp_1_1EntityReference.html", null ],
+      [ "xmlpp::exception", "classxmlpp_1_1exception.html", null ],
+      [ "exception", "classstd_1_1exception.html", null ],
+      [ "xmlpp::internal_error", "classxmlpp_1_1internal__error.html", null ],
+      [ "xmlpp::KeepBlanks", "classxmlpp_1_1KeepBlanks.html", null ],
+      [ "xmlpp::Node", "classxmlpp_1_1Node.html", null ],
+      [ "xmlpp::NonCopyable", "classxmlpp_1_1NonCopyable.html", null ],
+      [ "xmlpp::OStreamOutputBuffer", "classxmlpp_1_1OStreamOutputBuffer.html", null ],
+      [ "xmlpp::OutputBuffer", "classxmlpp_1_1OutputBuffer.html", null ],
+      [ "xmlpp::parse_error", "classxmlpp_1_1parse__error.html", null ],
+      [ "xmlpp::Parser", "classxmlpp_1_1Parser.html", null ],
+      [ "xmlpp::ProcessingInstructionNode", "classxmlpp_1_1ProcessingInstructionNode.html", null ],
+      [ "xmlpp::SaxParser", "classxmlpp_1_1SaxParser.html", null ],
+      [ "xmlpp::TextNode", "classxmlpp_1_1TextNode.html", null ],
+      [ "xmlpp::validity_error", "classxmlpp_1_1validity__error.html", null ]
+    ] ],
+    [ "Class Index", "classes.html", null ],
+    [ "Class Hierarchy", "hierarchy.html", [
+      [ "xmlpp::SaxParser::Attribute", "structxmlpp_1_1SaxParser_1_1Attribute.html", null ],
+      [ "xmlpp::SaxParser::AttributeHasName", "structxmlpp_1_1SaxParser_1_1AttributeHasName.html", null ],
+      [ "xmlpp::Dtd", "classxmlpp_1_1Dtd.html", null ],
+      [ "exception", "classstd_1_1exception.html", [
+        [ "xmlpp::exception", "classxmlpp_1_1exception.html", [
+          [ "xmlpp::internal_error", "classxmlpp_1_1internal__error.html", null ],
+          [ "xmlpp::parse_error", "classxmlpp_1_1parse__error.html", [
+            [ "xmlpp::validity_error", "classxmlpp_1_1validity__error.html", null ]
+          ] ]
+        ] ]
+      ] ],
+      [ "xmlpp::KeepBlanks", "classxmlpp_1_1KeepBlanks.html", null ],
+      [ "xmlpp::NonCopyable", "classxmlpp_1_1NonCopyable.html", [
+        [ "xmlpp::Document", "classxmlpp_1_1Document.html", null ],
+        [ "xmlpp::Node", "classxmlpp_1_1Node.html", [
+          [ "xmlpp::Attribute", "classxmlpp_1_1Attribute.html", null ],
+          [ "xmlpp::ContentNode", "classxmlpp_1_1ContentNode.html", [
+            [ "xmlpp::CdataNode", "classxmlpp_1_1CdataNode.html", null ],
+            [ "xmlpp::CommentNode", "classxmlpp_1_1CommentNode.html", null ],
+            [ "xmlpp::ProcessingInstructionNode", "classxmlpp_1_1ProcessingInstructionNode.html", null ],
+            [ "xmlpp::TextNode", "classxmlpp_1_1TextNode.html", null ]
+          ] ],
+          [ "xmlpp::Element", "classxmlpp_1_1Element.html", null ],
+          [ "xmlpp::EntityReference", "classxmlpp_1_1EntityReference.html", null ]
+        ] ],
+        [ "xmlpp::OutputBuffer", "classxmlpp_1_1OutputBuffer.html", [
+          [ "xmlpp::OStreamOutputBuffer", "classxmlpp_1_1OStreamOutputBuffer.html", null ]
+        ] ],
+        [ "xmlpp::Parser", "classxmlpp_1_1Parser.html", [
+          [ "xmlpp::DomParser", "classxmlpp_1_1DomParser.html", null ],
+          [ "xmlpp::SaxParser", "classxmlpp_1_1SaxParser.html", null ]
+        ] ]
+      ] ]
+    ] ],
+    [ "Class Members", "functions.html", null ],
+    [ "Namespace List", "namespaces.html", [
+      [ "xmlpp", "namespacexmlpp.html", null ]
+    ] ],
+    [ "Namespace Members", "namespacemembers.html", null ],
+    [ "File List", "files.html", [
+      [ "attribute.h", "attribute_8h.html", null ],
+      [ "cdatanode.h", "cdatanode_8h.html", null ],
+      [ "commentnode.h", "commentnode_8h.html", null ],
+      [ "contentnode.h", "contentnode_8h.html", null ],
+      [ "document.h", "document_8h.html", null ],
+      [ "domparser.h", "domparser_8h.html", null ],
+      [ "dtd.h", "dtd_8h.html", null ],
+      [ "element.h", "element_8h.html", null ],
+      [ "entityreference.h", "entityreference_8h.html", null ],
+      [ "exception.h", "exception_8h.html", null ],
+      [ "internal_error.h", "internal__error_8h.html", null ],
+      [ "keepblanks.h", "keepblanks_8h.html", null ],
+      [ "libxml++.h", "libxml_09_09_8h.html", null ],
+      [ "node.h", "node_8h.html", null ],
+      [ "noncopyable.h", "noncopyable_8h.html", null ],
+      [ "ostreamoutputbuffer.h", "ostreamoutputbuffer_8h.html", null ],
+      [ "outputbuffer.h", "outputbuffer_8h.html", null ],
+      [ "parse_error.h", "parse__error_8h.html", null ],
+      [ "parser.h", "parser_8h.html", null ],
+      [ "processinginstructionnode.h", "processinginstructionnode_8h.html", null ],
+      [ "saxparser.h", "saxparser_8h.html", null ],
+      [ "textnode.h", "textnode_8h.html", null ],
+      [ "validity_error.h", "validity__error_8h.html", null ]
+    ] ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
